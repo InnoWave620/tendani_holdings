0ea64f0b4ca488a11adedc78c529ca2332452ddd
Senzo Ngema <ngemasenzo60@gmail.com>
2025-08-07 18:36:21 +0000

    2025-08-07T18:34:39.536978Z	Cloning repository... 2025-08-07T18:34:42.66

--- next.config.mjs
+++ next.config.mjs
@@ -2,23 +2,8 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
